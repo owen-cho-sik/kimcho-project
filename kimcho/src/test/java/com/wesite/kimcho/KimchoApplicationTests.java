9561ab3750a58cbfc50391d6bc2419be4a0ce90f
kimsungeun <sungeunkim222@gmail.com>
2024-10-30 20:43:32 +0900

    start spring boot

---
+++ kimcho/src/test/java/com/wesite/kimcho/KimchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesite.kimcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
